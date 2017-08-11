668507c3dfcd29ec07249d5ecbfcb71a51dac48a
TuanGeek <leon.zweig@gmail.com>
2017-08-11 16:09:51 -0400

    changed 'wrong_asnwer' key to be 'wrong_answers' key in database

--- Tests/test_database.py
+++ Tests/test_database.py
@@ -22,37 +22,46 @@ class TestJsonDatabase(unittest.TestCase):
 
             '''
             database format for reference:
+                
                 {
-                    "Category": {
-                        "doubleJeopardy": {
-                            "category1": {
+                    "doubleJeopardy": [
+                        {
+                            "category_name": "Solar System", 
+                            "category_number": "category1", 
+                            "questions": [
                                 ...
-                            }
+                            ]
+                        },
+                        {
+                            ...
                         }
-                        "singleJeopardy": {
-                            "category1": {
+                    ],
+                    "singleJeopardy": [
+                        {
+                            "category_name": "Solar System", 
+                            "category_number": "category1", 
+                            "questions": [
                                 ...
-                            }
+                            ]
                         }
-                    }
+                    ]
+                }
+                
             '''
 
             #check if database has Category keyword and make sure it is not empty
-            self.assertTrue('Category' in self.database.keys())
-            self.database_jeopardy_rounds = self.database['Category']
-            self.assertIsNotNone(self.database_jeopardy_rounds)
-
+            rounds = self.database.keys()
             # make sure has both a double and a single jeopardy
-            self.assertEqual(len(self.database_jeopardy_rounds), 2)
+            self.assertEqual(len(rounds), 2)
 
             #check if doubleJeopardy exist
-            self.assertTrue('doubleJeopardy' in self.database_jeopardy_rounds.keys())
-            self.database_double_jeopardy = self.database_jeopardy_rounds['doubleJeopardy']
+            self.assertTrue('doubleJeopardy' in rounds)
+            self.database_double_jeopardy = self.database['doubleJeopardy']
             self.assertIsNotNone(self.database_double_jeopardy)
 
             #check if singlJeopardy exists
-            self.assertTrue('singleJeopardy' in self.database_jeopardy_rounds.keys())
-            self.database_single_jeopardy = self.database_jeopardy_rounds['singleJeopardy']
+            self.assertTrue('singleJeopardy' in rounds)
+            self.database_single_jeopardy = self.database['singleJeopardy']
             self.assertIsNotNone(self.database_single_jeopardy)
         else:
             self.assertTrue(os.path.exists(self.database_path))
@@ -77,103 +86,98 @@ class TestJsonDatabase(unittest.TestCase):
     #     self.assertTrue('doubleJeopardy' in database['Category'].keys())
     #     self.assertTrue('singleJeopardy' in database['Category'].keys())
 
-    #check if database has at least one doubleJeopardy
-    def test_has_doubleJeopardy(self):
+    #check if database has at least one doubleJeopardy and check it has category_number, category_name, and questions
+    def test_has_doubleJeopardy_categories(self):
         #make sure doubleJeopardy has at least one category in it and
         self.assertIsNotNone(self.database_double_jeopardy)
-        self.assertEqual(len(self.database_double_jeopardy),1)
+        self.assertGreaterEqual(len(self.database_double_jeopardy),1)
 
-        #get the first category of double jeopardy
-        double_jeopardy_categories = self.database_double_jeopardy.keys()
-        self.assertIsNotNone(double_jeopardy_categories)
-        double_jeopardy_first_category = self.database_double_jeopardy[double_jeopardy_categories[0]]
-        self.assertIsNotNone(double_jeopardy_first_category)
-        double_jeopardy_first_category_name = double_jeopardy_first_category.keys()[0]
-
-        #check questions has at least 5 questions
-        questions = double_jeopardy_first_category[double_jeopardy_first_category_name]
-        self.assertGreaterEqual(len(questions), 5)
-
-        for q_number in questions:
-            question = questions[q_number]['question']
-            #check has one question and is a string
-            self.assertTrue(isinstance(question,str) or isinstance(question,unicode) )
-            #check has one answer
-            answer = questions[q_number]['question']
-            self.assertTrue(isinstance(answer, str) or isinstance(answer, unicode))
-            #check has at least 4 wrong answers
-            self.assertGreaterEqual(len(questions[q_number]['wrong_answers']), 4)
+        #check each category in double_jeopardy has a category_number, category_name, and questions
+        double_categories = self.database_double_jeopardy
+        for double_category in double_categories:
+            #make sure that category has a number
+            self.assertIsNotNone(double_category['category_number'])
+            #make sure the category has category_name
+            self.assertIsNotNone(double_category['category_name'])
+            #make sure the category has questions
+            self.assertIsNotNone(double_category['questions'])
 
 
-        # check if database has at least one singleJeopardy
-        def test_has_singleJeopardy(self):
-            # make sure doubleJeopardy has at least one category in it and
-            self.assertIsNotNone(self.database_single_jeopardy)
-            self.assertGreaterEqual(len(self.database_single_jeopardy), 5)
+    # check if database has at least one singleJeopardy and check it has category_number, category_name, and questions
+    def test_has_singleJeopardy_categories(self):
+        # make sure doubleJeopardy has at least one category in it and
+        self.assertIsNotNone(self.database_single_jeopardy)
+        self.assertGreaterEqual(len(self.database_single_jeopardy), 1)
 
-            # get the first category of double jeopardy
-            single_jeopardy_categories =self.database_single_jeopardy.keys()
-            self.assertIsNotNone(single_jeopardy_categories)
-            single_jeopardy_first_category = self.database_single_jeopardy[single_jeopardy_categories[0]]
-            self.assertIsNotNone(single_jeopardy_first_category)
-            single_jeopardy_first_category_name = single_jeopardy_first_category.keys()[0]
+        # check each category in single_jeopardy has a category_number, category_name, and questions
+        single_categories = self.database_single_jeopardy
+        for single_category in single_categories:
+            # make sure that category has a number
+            self.assertIsNotNone(single_category['category_number'])
+            # make sure the category has category_name
+            self.assertIsNotNone(single_category['category_name'])
+            # make sure the category has questions
+            self.assertIsNotNone(single_category['questions'])
 
-            # check questions has at least 5 questions
-            questions = single_jeopardy_first_category[single_jeopardy_first_category_name]
-            self.assertGreaterEqual(len(questions), 5)
 
-            for q_number in questions:
-                question = questions[q_number]['question']
-                # check has one question and is a string
-                self.assertTrue(isinstance(question, str) or isinstance(question, unicode))
-                # check has one answer
-                answer = questions[q_number]['question']
-                self.assertTrue(isinstance(answer, str) or isinstance(answer, unicode))
-                # check has at least 4 wrong answers
-                self.assertGreaterEqual(len(questions[q_number]['wrong_answers']), 4)
-
+    #check that all categories has at least 5 questions, each question has the question, answer, and 4+ wrong answers
     def test_has_all_questions_has_answer_and_wrong_answers(self):
         '''
-        database format for reference:
+        database question format for reference:
+            {
+                ...
+                {
+                    "answer": "Neptune",
+                    "number": "question5",
+                    "question": "This panet is the coolest in our solar system.",
+                    "wrong_answer": [
+                        "Uranus",
+                        "Pluto",
+                        "Saturn",
+                        "Jupiter",
+                        "Mercury",
+                        "Venus",
+                        "Sun",
+                        "Mars",
+                        "Earth",
+                        "Moon",
+                        "Triton",
+                        "Ceres",
+                        "Milky Way",
+                        "Eris",
+                        "Titan"
+                },
 
-            "doubleJeopardy": {
-                "category1": {
-                    "Solar System": {
-                        "question1": {
-                            ...
-                        }
-                        "question2": {
-                            ...
-                        }
-                    }
-                }
-            }
-            "singleJeopardy": {
-                "category1": {
-                    "Potent Potables": {
-                        "question1": {
-                            ...
-                        }
-                        "question2": {
-                            ...
-                        }
-                    }
-                }
-            }
+                ...
         '''
 
-        #get categories number for double_jeopardy
-        double_jeopardy_categories = self.database_double_jeopardy.keys()
-        for category_number in double_jeopardy_categories:
-            #check has at least 5 questions in each category
-            number_questions = len(self.database_double_jeopardy[category_number])
-            print(number_questions)
-        # get categories number for single_jeopardy
-        single_jeopardy_categories = self.database_single_jeopardy.keys()
-        for category_number in single_jeopardy_categories:
-            # check has at least 5 questions in each category
-            number_questions = len(self.database_single_jeopardy[category_number])
-            print(number_questions)
+        #get questions for all double_jeopardy and single_jeopardy
+        for rounds in self.database.keys():
+            for category in self.database[rounds]:
+                #make sure category has questions
+                self.assertTrue('questions' in category)
+                questions = category['questions']
+                # make sure it has at least 5 questions
+                self.assertGreaterEqual(len(questions), 5)
+
+                for question in questions:
+                    #make sure question has the question, answer, and wrong_answers
+                    self.assertTrue('question' in question)
+                    self.assertTrue('answer' in question)
+                    self.assertTrue('wrong_answers' in question)
+                    
+            #get categories number for double_jeopardy
+            double_jeopardy_categories = self.database_double_jeopardy.keys()
+            for category_number in double_jeopardy_categories:
+                #check has at least 5 questions in each category
+                number_questions = len(self.database_double_jeopardy[category_number])
+                print(number_questions)
+            # get categories number for single_jeopardy
+            single_jeopardy_categories = self.database_single_jeopardy.keys()
+            for category_number in single_jeopardy_categories:
+                # check has at least 5 questions in each category
+                number_questions = len(self.database_single_jeopardy[category_number])
+                print(number_questions)
 
 
     def test_has_atleast_10_category(self):
